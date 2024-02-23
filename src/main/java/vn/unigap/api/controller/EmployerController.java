1a6ca77159186e266d88a875bcf367d33508e8f1
lppduy <lephamphuongduy@gmail.com>
2024-02-23 20:17:29 +0700

    Implemented dummy api - basic CRUD

--- src/main/java/vn/unigap/api/controller/EmployerController.java
+++ src/main/java/vn/unigap/api/controller/EmployerController.java
@@ -1,34 +1,123 @@
 package vn.unigap.api.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import vn.unigap.api.entity.Employer;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("${api.prefix}/employers")
 public class EmployerController {
 
+
     @PostMapping
-    public String createEmployer() {
-        return "Created new employer successfully.";
+    public ResponseEntity<Employer> createEmployer(@RequestBody Employer employer) {
+
+        Employer employer1 = new Employer(employer.getId(),employer.getEmail(),
+                employer.getName(),
+                employer.getProvince(),
+                employer.getDescription());
+
+
+        return ResponseEntity.ok(employer1);
     }
 
     @PutMapping("/{id}")
-    public String updateEmployer(@PathVariable("id") int id) {
-        return "Updated successfully the employer with id = " + id;
+    public ResponseEntity<?>  updateEmployer(@PathVariable("id") int id, @RequestBody Employer employer) {
+
+        Employer employer1 = new Employer(1, "employer1@example.com", "Employer 1", 1, "Description 1");
+        Employer employer2 = new Employer(2, "employer2@example.com", "Employer 2", 2, "Description 2");
+        Employer employer3 = new Employer(3, "employer3@example.com", "Employer 3", 3, "Description 3");
+
+        List<Employer> employers = new ArrayList<>();
+
+        employers.add(employer1);
+        employers.add(employer2);
+        employers.add(employer3);
+
+        try {
+            Employer employerToUpdate = employers.get(id - 1);
+
+            employerToUpdate.setEmail(employer.getEmail());
+            employerToUpdate.setName(employer.getName());
+            employerToUpdate.setDescription(employer.getDescription());
+            employerToUpdate.setProvince(employer.getProvince());
+            employerToUpdate.setDescription(employer.getDescription());
+            employerToUpdate.setUpdatedAt(LocalDateTime.now());
+
+            return ResponseEntity.ok(employerToUpdate);
+
+        } catch (IndexOutOfBoundsException e) {
+            String msg = "Can not found the user with id = " + id;
+            return new ResponseEntity<>(msg, HttpStatus.NOT_FOUND);
+        }
+
     }
 
     @GetMapping("/{id}")
-    public String getEmployerById(@PathVariable("id") int id) {
-     return "The employer with id = " + id;
+    public ResponseEntity<?> getEmployerById(@PathVariable("id") int id) {
+
+        Employer employer1 = new Employer(1, "employer1@example.com", "Employer 1", 1, "Description 1");
+        Employer employer2 = new Employer(2, "employer2@example.com", "Employer 2", 2, "Description 2");
+        Employer employer3 = new Employer(3, "employer3@example.com", "Employer 3", 3, "Description 3");
+
+        List<Employer> employers = new ArrayList<>();
+
+        employers.add(employer1);
+        employers.add(employer2);
+        employers.add(employer3);
+        int realId = id - 1;
+
+        try {
+            Employer employerToFind = employers.get(id - 1);
+            return ResponseEntity.ok(employerToFind);
+        } catch (IndexOutOfBoundsException e) {
+            String msg = "Can not found the user with id = " + id;
+            return new ResponseEntity<>(msg, HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping
-    public String getAllEmployers() {
-        return "A list of all employers";
+    public ResponseEntity<List<Employer>> getAllEmployers() {
+
+        Employer employer1 = new Employer(1, "employer1@example.com", "Employer 1", 1, "Description 1");
+        Employer employer2 = new Employer(2, "employer2@example.com", "Employer 2", 2, "Description 2");
+        Employer employer3 = new Employer(3, "employer3@example.com", "Employer 3", 3, "Description 3");
+
+        List<Employer> employers = new ArrayList<>();
+        employers.add(employer1);
+        employers.add(employer2);
+        employers.add(employer3);
+
+        return ResponseEntity.ok(employers);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteEmployer(@PathVariable("id") int id) {
-     return "Deleted successfully the employer with id = " + id;
+    public ResponseEntity<String> deleteEmployer(@PathVariable("id") int id) {
+
+        Employer employer1 = new Employer(1, "employer1@example.com", "Employer 1", 1, "Description 1");
+        Employer employer2 = new Employer(2, "employer2@example.com", "Employer 2", 2, "Description 2");
+        Employer employer3 = new Employer(3, "employer3@example.com", "Employer 3", 3, "Description 3");
+
+        List<Employer> employers = new ArrayList<>();
+
+        employers.add(employer1);
+        employers.add(employer2);
+        employers.add(employer3);
+        int realId = id - 1;
+
+        try {
+            Employer employerToFind = employers.get(id - 1);
+            return ResponseEntity.ok("Deleted successfully the employer with id = " + id);
+        } catch (IndexOutOfBoundsException e) {
+            String msg = "Can not found the user with id = " + id;
+            return new ResponseEntity<>(msg, HttpStatus.NOT_FOUND);
+        }
+
     }
 
 }
