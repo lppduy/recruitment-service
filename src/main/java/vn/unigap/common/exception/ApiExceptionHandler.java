d9a63bab8b3620d1a37dd475325a516e8d0e0bee
lppduy <lephamphuongduy@gmail.com>
2024-03-28 18:20:46 +0700

    feat: config logging, add security feature

--- src/main/java/vn/unigap/common/exception/ApiExceptionHandler.java
+++ src/main/java/vn/unigap/common/exception/ApiExceptionHandler.java
@@ -8,13 +8,23 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import vn.unigap.common.response.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ControllerAdvice
 @Order(value = Ordered.HIGHEST_PRECEDENCE)
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+
+    private static final Logger logger = LoggerFactory.getLogger(ApiExceptionHandler.class);
+
     @ExceptionHandler(value = ApiException.class)
     public ResponseEntity<?> handleApiException(ApiException e) {
+        if (e.getHttpStatus().is4xxClientError()) {
+            logger.debug("Client error: ", e);
+        } else if (e.getHttpStatus().is5xxServerError()) {
+            logger.error("Server error: ", e);
+        }
         return responseEntity(e.getErrorCode(), e.getHttpStatus(), e.getMessage());
     }
 
